e468ee5b6a43b7df76bee9ae52850a513d855cf7
franciscoyaya <franciscoperez@ceitartessos.com>
2019-10-21 19:24:22 +0200

    chustonfeudal

---
+++ Unity/Assets/minombre3.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class minombre3 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Francisco Perez");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (21);
+	}
+}
